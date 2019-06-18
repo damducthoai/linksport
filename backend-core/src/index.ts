be97fe074ecb825129da6fb067d06712ef7718c8
thoai.nguyen <nguyendamducthoai>
2019-06-18 17:02:11 +0700

    demo

--- backend-core/src/index.ts
+++ backend-core/src/index.ts
@@ -1,8 +1,16 @@
 import * as http from 'http';
 
 const server = http.createServer((req, res) => {
-  res.write('ahihi');
-  res.end('chao cac ban');
+
+ 
+
+  req.on('data', (chunk) => {
+    const regBody: Uint8Array[] = [];
+    regBody.push(chunk);
+    const regBodyJson = Buffer.concat(regBody).toString();
+    res.end('data received')
+  })
+
 });
 server.on('clientError', (err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
