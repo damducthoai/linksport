daf6e7bdab4d90759cfd677fbcb4a612139f0d8c
nguyendamducthoai <nguyendamducthoai@gmail.com>
2019-06-20 23:44:56 +0700

    http suport

--- backend-core/src/http-verticle.ts
+++ backend-core/src/http-verticle.ts
@@ -1,10 +1,13 @@
 import { CoreVerticle } from "backend-base/lib/index";
+import * as events from 'events';
 import * as http from 'http';
 import { Validator } from 'jsonschema';
 
 
 
 export class HttpVerticle extends CoreVerticle {
+    
+    
     private readonly port: number;
     private readonly validator: Validator;
     private readonly eventBinding: any;
@@ -13,8 +16,8 @@ export class HttpVerticle extends CoreVerticle {
     /**
      *
      */
-    constructor(config: any) {
-        super(config, "HttpVerticle");
+    constructor(config: any, globalEvents: events) {
+        super(config, "HttpVerticle", globalEvents);
         this.port = this.config.port as number;
         this.eventBinding = this.config.eventBinding;
         this.validator = new Validator();
@@ -22,24 +25,30 @@ export class HttpVerticle extends CoreVerticle {
         this.allowUrl = this.config.allowUrl;
         this.registerValidator();
     }
+    protected onEvent(event: string, globalEvents: events): void {
+        throw new Error("Method not implemented.");
+    }
     protected onInit(){
         return new Promise<number>(res => {
             const server = http.createServer((req, response) => {
-                if(this.allowMethod !== req.method || this.allowUrl !== req.url) {
-                    response.end('method not support');
-                    return;
-                }
-                // TODO - handle empty body
-                req.on('data', async (chunk) => {
-                  const regBody: Uint8Array[] = [];
-                  regBody.push(chunk);
-                  const regBodyString = Buffer.concat(regBody).toString();
-                  const regBodyJson = JSON.parse(regBodyString);
-                  const schema = this.eventBinding.login.schema
-                  const validationResult = this.validator.validate(regBodyJson, schema).valid
-                  this.info(`validate event ${schema}: ${validationResult.toString()}`)
-                  response.end(`validate event ${schema}: ${validationResult.toString()}`)
-                })
+                this.globalEvents.emit('register_queue_dev', 'test');
+                response.end('test');
+                this.info('receive request http')
+                // if(this.allowMethod !== req.method || this.allowUrl !== req.url) {
+                //     response.end('method not support');
+                //     return;
+                // }
+                // // TODO - handle empty body
+                // req.on('data', async (chunk) => {
+                //   const regBody: Uint8Array[] = [];
+                //   regBody.push(chunk);
+                //   const regBodyString = Buffer.concat(regBody).toString();
+                //   const regBodyJson = JSON.parse(regBodyString);
+                //   const schema = this.eventBinding.login.schema
+                //   const validationResult = this.validator.validate(regBodyJson, schema).valid
+                //   this.info(`validate event ${schema}: ${validationResult.toString()}`)
+                //   response.end(`validate event ${schema}: ${validationResult.toString()}`)
+                // })
               });
             server.on('clientError', (err, socket) => {
                 socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
