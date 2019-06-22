efcea969fa9920c0294f4e47479b1ee228678c15
nguyendamducthoai <nguyendamducthoai@gmail.com>
2019-06-22 16:08:30 +0700

    get config

--- backend-handler/src/user/RegisterHandler.ts
+++ backend-handler/src/user/RegisterHandler.ts
@@ -10,13 +10,14 @@ export class RegisterHandler extends RpcServer {
     private readonly idGenerator: Snowflake;
     private readonly pgSetting : any;
     private readonly pool: any;
-    private readonly saltRounds = 10;
+    private readonly saltRounds: number;
 
     constructor(config: any, protected globalEvents: events, extraConfigKeys?: string[]) {
         super(config, "RegisterHandler", globalEvents, extraConfigKeys);
         this.idGenerator = new Snowflake();
         this.pgSetting = this.config.pgSetting;
         this.pool = new Pool(this.pgSetting);
+        this.saltRounds = this.config.saltRounds;
     }
     public onMessage(message: string): Promise<string>{
         const curId = this.idGenerator.generate();
